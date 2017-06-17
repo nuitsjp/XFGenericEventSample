65e52465820b39732f93723c5317320974587059
nuits.jp <nuits.jp@live.jp>
2017-06-17 15:40:37 +0900

    Add Project.

---
+++ XFGenericEventSample/XFGenericEventSample/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFGenericEventSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			//MyNotificator.Notified += OnNotified;
+		}
+
+		private void OnNotified(object sender, NotificationEventArgs<string> e)
+		{
+			MyLabel.Text = e.Message;
+		}
+
+		private void Button_OnClicked(object sender, EventArgs e)
+		{
+			MyNotificator.NotifyMessage("Message.");
+		}
+	}
+}
---
+++ XFGenericEventSample/XFGenericEventSample/Notificator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFGenericEventSample
+{
+public class Notificator<T>
+{
+	public event EventHandler<NotificationEventArgs<T>> Notified;
+
+	public void NotifyMessage(T message)
+	{
+		Notified?.Invoke(this, new NotificationEventArgs<T>{ Message = message});
+	}
+}
+}
